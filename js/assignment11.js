7e3b503473c4d6dedd3f4a99d5bdcb5281a16b1f
jddouthit3 <jodo6486@colorado.edu>
2022-04-02 13:40:27 -0600

    Organizational changes

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,8 +1,7 @@
 const Kanye = document.getElementById('Kanye');
 console.log("Kanye");
 const Tyler = document.getElementById('Tyler');
-const Will = document.getElementById('Will');
-const Chris = document.getElementById('Chris');
+const para = document.querySelector('p');
 
 Kanye.addEventListener('click', function onClick(event) {
   document.body.style.backgroundImage = "url('../img/Kanye.png')";
@@ -13,6 +12,15 @@ Tyler.addEventListener('click', function onClick(event) {
   document.body.style.backgroundImage ="url('../img/Tyler.png')";
 });
 
-Will.addEventListener('click', function onClick(event) {
-  let result = document.text.strike();
-});
+para.addEventListener('click',fightWin);
+
+function fightWin () {
+  let name = prompt('Who wins? Choose carefully.');
+  para.textContent = `I think that ${name} will win`;
+  console.log(name);
+  if(name === 'Will Smith') {
+    alert('Just pure wrong.');
+  } else if (name ==='Chris Rock') {
+    alert('Excellent selection. My money is on him too.');
+  }
+}
